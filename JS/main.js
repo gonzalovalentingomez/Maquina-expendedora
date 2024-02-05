9040baae23a97a1dbad98e89ea0c5a37b82ad4a2
Gonzalo Valentin Gomez <128619944+gonzalovalentingomez@users.noreply.github.com>
2024-02-05 18:18:32 -0300

    agregando el bucle

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,16 @@
-//Precios de las bebidas
+//P recios de las bebidas
 const precios = {
     "1": 2500,
     "2": 1800,
     "3": 3000,
-    "4": 3500,
+    "4": 2800,
+    "5": 3200,
 };
 
-//Función para mostrar el menú
+//Función para mostrar el menú y obtener la selección del usuario
 function mostrarMenu() {
     return prompt(
-        "Selecciona una bebida \n 1- Fernet con Coca: $2500 \n 2- Cerveza: $1800 \n 3- Gin Tonic: $3000 \n 4- Mojito: $3500"
+        "Selecciona una bebida \n 1- Fernet con Coca: $2500 \n 2- Cerveza: $1800 \n 3- Gin Tonic: $3000 \n 4- Mojito: $2800 \n 5- Caipiroska: $3200 \n 0- Salir"
     );
 }
 
@@ -21,25 +22,38 @@ function procesarPago(bebida, precio, montoPagado) {
 
 //Función principal
 function ordenarBebida() {
-    const seleccion = mostrarMenu();
+    let continuar = true;
 
-    if (precios[seleccion]) {
-        const bebida = seleccion === "1" ? "Fernet con Coca" : seleccion === "2" ? "Cerveza" : seleccion === "3" ? "Gin Tonic" : "Mojito";
-        const precio = precios[seleccion];
+    while (continuar) {
+        const seleccion = mostrarMenu();
 
-        //Solicitar monto con el que va a pagar
-        let montoPagado = parseFloat(prompt("El total a pagar es $" + precio + ". Ingrese el monto con el que va a pagar:"));
+        if (seleccion === "0") {
+            alert("Gracias por tu visita. ¡Hasta luego!");
+            continuar = false;
+        } else if (precios[seleccion]) {
+            const bebida = seleccion === "1" ? "Fernet con Coca" :
+                            seleccion === "2" ? "Cerveza" :
+                            seleccion === "3" ? "Gin Tonic" :
+                            seleccion === "4" ? "Mojito" :
+                            "Caipiroska";
 
-        //Validar que lo pagado sea válido
-        if (montoPagado < precio) {
-            alert("Monto no válido. Pago cancelado.");
+            const precio = precios[seleccion];
+
+            //Monto con el que va a pagar
+            let montoPagado = parseFloat(prompt("El total a pagar es $" + precio + ". Ingrese el monto con el que va a pagar:"));
+
+            //Valida que el monto pagado sea válido
+            if (montoPagado < precio) {
+                alert("Monto no válido. Pago cancelado.");
+            } else {
+                procesarPago(bebida, precio, montoPagado);
+            }
         } else {
-            procesarPago(bebida, precio, montoPagado);
+            alert("No seleccionaste una bebida válida");
         }
-    } else {
-        alert("No seleccionaste una bebida válida");
     }
 }
 
-//Función principal
+//Llamando a la función principal
 ordenarBebida();
+
